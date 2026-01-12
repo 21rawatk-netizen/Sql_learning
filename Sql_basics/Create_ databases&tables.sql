a7a623b6c77f4b08f697254f10b19c2863b8e6a0
21rawatk-netizen <21rawatk@gmail.com>
2026-01-12 23:37:34 +0530

    Create Create_ databases&tables.sql

---
+++ Sql_basics/Create_ databases&tables.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE company_db;
+USE company_db;
+
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    department VARCHAR(50),
+    salary INT,
+    age INT
+);
+
+CREATE TABLE departments (
+    dept_id INT PRIMARY KEY,
+    dept_name VARCHAR(50)
+);
