f0ce564079a1d84a650c82e72f37cc0f9bfa6f61
21rawatk-netizen <21rawatk@gmail.com>
2026-01-12 23:39:16 +0530

    Create Insert_select_data.sql

---
+++ Sql_basics/Insert_select_data.sql
@@ -0,0 +1,17 @@
+
+
+INSERT INTO employees VALUES
+(1, 'Amit', 'IT', 60000, 25),
+(2, 'Neha', 'HR', 50000, 28),
+(3, 'Rahul', 'IT', 70000, 30),
+(4, 'Priya', 'Finance', 65000, 32),
+(5, 'Ankit', 'HR', 48000, 26);
+
+INSERT INTO departments VALUES
+(1, 'IT'),
+(2, 'HR'),
+(3, 'Finance');
+
+
+SELECT * FROM employees;
+SELECT name, salary FROM employees;
