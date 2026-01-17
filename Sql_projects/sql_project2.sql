a1d6973a1e385e8807116146661b7058f3d4b7fc
21rawatk-netizen <21rawatk@gmail.com>
2026-01-17 23:51:58 +0530

    Online Shopping Sales Analysis

--- Sql_projects/sql_project2.sql
+++ Sql_projects/sql_project2.sql
@@ -12,6 +12,16 @@ CREATE TABLE orders (
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
+INSERT INTO customers VALUES
+(1,'Kiran'),
+(2,'Rohit'),
+(3,'Anjali');
+
+INSERT INTO orders VALUES
+(101,1,5000,'2024-01-10'),
+(102,1,3000,'2024-01-15'),
+(103,2,2000,'2024-01-20'),
+(104,3,9000,'2024-01-25');
 
 SELECT customer_id, SUM(order_amount) AS total_spent
 FROM orders
