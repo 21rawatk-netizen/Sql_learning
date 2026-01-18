0c882b056ed3be2705c66a529781f46e7964e492
21rawatk-netizen <21rawatk@gmail.com>
2026-01-18 23:47:22 +0530

    Create DISTINCT_data.sql

---
+++ Sql_basics/DISTINCT_data.sql
@@ -0,0 +1,7 @@
+/*it selects the statements that has the unique values in the data base*/
+SELECT * FROM Customers
+WHERE country='Mexico'p;
+SELECT * FROM Customers
+WHERE customerID=1;
+SELECT * FROM Customers
+WHERE CustomerID>80;
